868c0d42fe9d87d6876b3817390baf4ef032fadd
justine.bunag <justinbunag1998@gmail.com>
2020-06-04 00:38:01 +0800

    Challenge #1 - Dynamic pages with templating

--- src/app.js
+++ src/app.js
@@ -20,7 +20,20 @@ app.get('/about', (req, res) => {
 		title : 'About me',
 		name : 'Andrew Mead'
 	})
-}) 
+})  
+
+
+// Goal : Create a template for help page 
+// 1. Setup a help template to render a help message to the screen 
+// 2. Setup the help route and render the template with an example message 
+// 3. Visit the route in the browser and see your help message print 
+
+app.get('/help', (req, res) => {
+	res.render('help', {
+		title : 'Help' ,
+		name : 'Andrew mead'
+	})
+})
 
 app.get('/weather', (req, res) => {
 	res.send({
