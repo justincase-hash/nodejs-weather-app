5b4bd8bc037cfb9f537693ca31d4f8d6debc7621
justine.bunag <justinbunag1998@gmail.com>
2020-06-03 23:58:07 +0800

    Serving up CSS, JS , IMAGES AND more

--- src/app.js
+++ src/app.js
@@ -7,12 +7,6 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 //for customoizing server - root of the sites
 app.use(express.static(publicDirectoryPath))
 
-// Goal : create two more html files 
-// 1. Create a html page for about with "About" title
-// 2. Create a html page for help with "Help" title
-// 3. Remove the old route handlers for both 
-// 4. Visit both in the browser to test your work 
-
 app.get('/weather', (req, res) => {
 	res.send({
 		'forecast' : 'It is snowing',
