d5e2cfee42b1fcd6e8d9fc6be588a586e075d648
justine.bunag <justinbunag1998@gmail.com>
2020-06-03 21:07:20 +0800

    express challenge #1

--- src/app.js
+++ src/app.js
@@ -3,13 +3,27 @@ const express = require('express')
 const app = express()
 
 app.get('', (req , res) => {
-    res.send('Hello Express!') 
+	res.send('Hello Express!') 
 })
 
 app.get('/help', (req, res) => {
-    res.send('Help Page')
+	res.send('Help Page')
+})
+
+app.get('/about' , (req, res) => {
+	res.send('About page')
+}) 
+
+app.get('/weather', (req, res) => {
+	res.send('Weather page')
 })
 
 app.listen(3000, () => {
-    console.log('Server is up on port 3000')
-})
+	console.log('Server is up on port 3000')
+})  
+
+
+// Goal : Setup two new routes 
+// 1. Set up an about route and render a page titile 
+// 2. Set up a weather route and render a page titile 
+// 3. Test your work by visiting both in the browser
