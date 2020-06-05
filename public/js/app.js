cb7b20cd3546f4e4bc87ccd4de75b1c236c966e6
justine.bunag <justinbunag1998@gmail.com>
2020-06-06 02:54:25 +0800

    Wiring up the User Interface

--- public/js/app.js
+++ public/js/app.js
@@ -2,30 +2,37 @@ console.log('Client Side Javascript File is loaded!')
 
 const weatherForm = document.querySelector('form') 
 const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')  
+const messageTwo = document.querySelector('#message-2')
+
+
+// Goal : Render conent to paragraphs 
+// 1. Select the second messsage p from JavaScript 
+// 2. Just before fetch, render loading message and empty p 
+// 3. If error , render error 
+// 4. if no error , render location and forecast
+// 5. Test your work ! search for errors and for valid locations 
 
 weatherForm.addEventListener('submit', (e) => { 
 	e.preventDefault()	
 	
 	const location  = search.value 
-	
+
+	messageOne.textContent = 'Loading...' 
+	messageTwo.textContent = ''
+
 	fetch('http://127.0.0.1:3000/weather?address='+ location).then( ( response ) => {
 		response.json().then( ( data ) => {
 				if	( data.error ){
-					console.log(data.error)
+					messageOne.textContent = data.error
 				} else {
-					console.log(data.location)
-					console.log(data.forecast)
+					messageOne.textContent = data.location
+					messageTwo.textContent = data.forecast
 				}
 		})
 	})
 
 }) 
-
-// Goal : Use input value to get weather 
-
-// 1. Migrate fetch call into the submit call back
-// 2. Use the search test as the address query string value 
-// 3. Submit the form with a valid and invalid value to test
 
 
 
